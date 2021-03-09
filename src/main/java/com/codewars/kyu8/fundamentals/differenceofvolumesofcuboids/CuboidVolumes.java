8ee0dabe50659e913727bd76021a9340c65d76dd
Yuriy <CB.Phantom@ya.ru>
2021-03-09 23:55:37 +0300

    Add Difference of Volumes of Cuboids.

---
+++ src/main/java/com/codewars/kyu8/fundamentals/differenceofvolumesofcuboids/CuboidVolumes.java
@@ -0,0 +1,14 @@
+package com.codewars.kyu8.fundamentals.differenceofvolumesofcuboids;
+
+/**
+ * @author Iuriy Gaydarzhi.
+ * @since 09.03.2021
+ */
+public class CuboidVolumes {
+
+    public static int findDifference(final int[] firstCuboid, final int[] secondCuboid) {
+
+
+        return 2021;
+    }
+}
