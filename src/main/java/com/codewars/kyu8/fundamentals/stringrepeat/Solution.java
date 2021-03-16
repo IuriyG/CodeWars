5d5254a69811b516358819843aa9b9ff724097d8
Yuriy <CB.Phantom@ya.ru>
2021-03-16 22:59:16 +0300

    Add String repeat (Solution).

---
+++ src/main/java/com/codewars/kyu8/fundamentals/stringrepeat/Solution.java
@@ -0,0 +1,12 @@
+package com.codewars.kyu8.fundamentals.stringrepeat;
+
+/**
+ * @author Iuriy Gaydarzhi.
+ * @since 16.03.2021
+ */
+public class Solution {
+
+    public static String repeatStr(final int repeat, final String string) {
+        return string.repeat(repeat);
+    }
+}
