22ea3a636c3cadd5ee0a6f2cecdb4a87c94fb68a
Yuriy <CB.Phantom@ya.ru>
2021-04-21 00:21:37 +0300

    Add Will you make it? (Solution).

--- src/test/java/com/codewars/kyu8/fundamentals/willyoumakeit/KataTest.java
+++ src/test/java/com/codewars/kyu8/fundamentals/willyoumakeit/KataTest.java
@@ -2,6 +2,8 @@ package com.codewars.kyu8.fundamentals.willyoumakeit;
 
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,9 +12,36 @@ import static org.junit.Assert.*;
  */
 public class KataTest {
 
+    public static boolean sol(double distanceToPump, double mpg, double fuelLeft) {
+        return distanceToPump <= mpg * fuelLeft;
+    }
+
     @Test
-    public void testSomething() {
+    public void sampleTest() {
         assertTrue(Kata.zeroFuel(50, 25, 2));
         assertFalse(Kata.zeroFuel(100, 50, 1));
+    }
+
+    @Test
+    public void basicTest() {
+        assertTrue(Kata.zeroFuel(60, 30, 3));
+        assertFalse(Kata.zeroFuel(70, 25, 1));
+        assertFalse(Kata.zeroFuel(100, 25, 3));
+    }
+
+    @Test
+    public void randomTest() {
+        Random rnd = new Random();
+
+        for (int i = 0; i < 100; ++i) {
+            int d = rnd.nextInt(91) + 10;
+            int m = rnd.nextInt(21) + 10;
+            int g = rnd.nextInt(5) + 1;
+
+            boolean expected = sol(d, m, g);
+            boolean actual = Kata.zeroFuel(d, m, g);
+
+            assertEquals(expected, actual);
+        }
     }
 }
