d2076c47a8d6d41769c4c4938f7d8ea0484f9610
Yuriy <CB.Phantom@ya.ru>
2021-05-01 23:09:01 +0300

    Fix some bugs.

--- src/main/java/com/codewars/kyu8/algorithms/milespergallontokilometersperliter/Converter.java
+++ src/main/java/com/codewars/kyu8/algorithms/milespergallontokilometersperliter/Converter.java
@@ -14,6 +14,6 @@ package com.codewars.kyu8.algorithms.milespergallontokilometersperliter;
 public class Converter {
 
     public static float mpgToKPM(final float mpg) {
-        return (float) (Math.round(((mpg / 4.54609188) * 1.609344) * 100) / 100d);
+        return (float) (Math.round(((mpg / 4.54609188f) * 1.609344f) * 100.0) / 100.0);
     }
 }
